41b206919b9035cf0a1173ed72095f6a4af8d3e5
reiFujise <niggfdjy@outlook.jp>
2022-12-12 12:34:41 +0900

    add sens test

---
+++ sens_test.cpp
@@ -0,0 +1,37 @@
+#include <mbed.h>
+#include "initializer.h"
+#include "motar.h"
+#include "reflector.h"
+
+int check_speed(Motar m){
+  if(m.speed > m.min_speed && m.speed < m.max_speed){
+    return m.speed;
+  }else if(m.speed < m.min_speed){
+    return m.min_speed;
+  }else if(m.speed > m.max_speed){
+    return m.max_speed;
+  }else{
+    return m.speed;
+  }
+}
+
+int main(){
+
+  float duty = 0.1f;
+
+  // インスタンス化
+  Motar m1;
+  Motar m2;
+	Reflector ref1;
+	Reflector ref2;
+
+  m1_phase = 1;
+  m2_phase = 1;
+
+  m1_enable.period(m1.period);
+  m2_enable.period(m2.period);
+
+  while(1){
+  }
+}
+
